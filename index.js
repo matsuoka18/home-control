31fea42b9377d605efdf2bb473ae1ddd449d2b2f
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2024-06-12 23:32:53 +0900

    index.js を更新

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ url = "https://script.google.com/macros/s/AKfycbxJ7duZLgYz0kI2pq2Xu6iIGNPFHV9e_p
 url2 = "https://matsuoka18.github.io/home-control/index.html";
 mode = "";
 battery_data = "";
+ani_branch = "OK";
 function data_road(){
     document.getElementById("all1").style.opacity = 1;
     document.getElementById("all1").style.display = "block";
     document.getElementById("all2").style.opacity = 0;
     document.getElementById("all2").style.display = "none";
+    ani();
 fetch(url,{
     "method":"get",
     "mode":"cors"
@@ -18,6 +20,7 @@ fetch(url,{
 })
 .then(resJson =>{
     //画面切り替え
+    ani_branch="FIN";
     document.getElementById("all1").style.opacity = 0;
     document.getElementById("all1").style.display = "none";
     document.getElementById("all2").style.opacity = 1;
@@ -144,3 +147,11 @@ function count2(){
     document.getElementById("time3").innerHTML = time_data+"sec";
 }
 //1800000
+
+function ani(){
+    if(ani_branch == "ok"){
+        console.log("strat")
+}else{
+    console.log("stop");
+    }
+}
